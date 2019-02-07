c57061090eb79bff18a34434794d89e4664cb2f3
Ryan Soto <rsoto003@ucr.edu>
2019-02-07 10:56:44 -0800

    auth is now handled

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+//Passport Config
+require('./config/passport')(passport);
 
+//Load Routes
+const auth = require('./routes/auth');
 const app = express();
 
-const port = process.env.PORT || 5000;
+
 
 //index route
 app.get('/', (req, res)=> {
     res.send('this is the index page.');
-})
+});
+//Use Routes
+app.use('/auth', auth);
 
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
